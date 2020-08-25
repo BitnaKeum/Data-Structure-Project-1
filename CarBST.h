f57e9ceb1528ae5d78ad980b88ccdb81e39cbbd1
BitnaKeum <beausty23@naver.com>
2020-08-25 23:01:45 +0900

    Update CarBST.h

--- CarBST.h
+++ CarBST.h
@@ -1,27 +1,122 @@
-#pragma once
-#include "CarNode.h"
-#include <iostream>
-#include <fstream>// header file declare
+#include "Queue.h"
+#include <fstream>
+
 using namespace std;
-class CarBST
+
+Queue::Queue()//constructor
 {
-public:
-	CarNode * root;	
-	CarNode * p;
-	CarNode * q;	// parent of p
+	pHead = NULL;
+	pTail = NULL;
+	pCur = NULL;
+	size = 0;
+}
 
-public:
-	CarBST();
-	~CarBST();
 
-	void		Insert(CarNode * node);			// LOAD, MOVE
-	CarNode *	Delete(int num);				// COMPLETE
-	CarNode *	Search(int num);				// ADD, COMPLETE, SEARCH
-	bool		Print(const char * order);		// PRINT
-	bool		Save();							// SAVE
-	void		Inorder(CarNode *node);
-	void		Preorder(CarNode *node);
-	void		Postorder(CarNode *node);
-	void		clear(CarNode* Root);
-};
+Queue::~Queue()//destructor
+{
+	// implemente Pop function until (size == 0)
+	while (size)
+		Pop();
+}
 
+
+
+void		Queue::Push(CarNode * node)			// LOAD
+{
+	// if there is node that has same car number with 'node' in queue
+	if(Search(node->GetcarNum()) != NULL) return;
+
+	if (pHead == NULL)
+	{
+		pHead = node;
+		pTail = node;
+		// If there is only one node in the queue, both pHead and pTail refer to that node.
+	}
+	else
+	{
+		pTail->SetNext(node);	// Connect the new node to the last node in the queue
+		pTail = node;
+	}
+
+	size++;
+}
+
+// function to pop the frontmost element
+void	Queue::Pop()
+{
+	pCur = pHead;
+
+	pHead = pCur->GetNext();	
+	// disconnect the first element from the queue
+
+	if (pHead == NULL) pTail = NULL;	//  queue is empty
+
+	size--;		// decrement the size of queue
+	delete(pCur);
+}
+
+CarNode *	Queue::Search(int num)				// SEARCH
+{
+	pCur = pHead;
+
+	while (pCur != NULL) {	// repeat until pCur is NULL 
+		if (pCur->GetcarNum() == num)	// if finds the node that has num, exit the loop
+			break;
+
+		pCur = pCur->GetNext();	// pCur points the next node
+	}
+
+	return pCur;	// return the address pCur points
+}
+
+// function to print the information of all nodes in queue
+bool		Queue::Print()						// PRINT
+{
+	if (pHead == NULL)	return false;
+
+	ofstream flog("log.txt", ios::app);
+
+	cout << "======== PRINT ========" << endl;
+	flog << "======== PRINT ========" << endl;
+
+	pCur = pHead;
+	while (pCur != NULL)
+	{
+		// print information of all nodes in queue 
+		cout << pCur->GetcarNum() << " " << pCur->GetcarOwner() << " "
+			<< pCur->Getstate() << endl;
+		// save in log.txt
+		flog << pCur->GetcarNum() << " " << pCur->GetcarOwner() << " "
+			<< pCur->Getstate() << endl;
+
+		pCur = pCur->GetNext();	// pCur points the next node
+	}
+
+	cout << "=======================" << endl << endl;
+	flog << "=======================" << endl << endl;
+
+	flog.close();
+
+	return true;
+}
+
+// function to save the data of all nodes 
+bool		Queue::Save()
+{
+	if (pHead == NULL)	return false;		// if there is no data in Queue, error occurs
+
+	ofstream wComplete("complete_list_car.txt");	
+
+	pCur = pHead;
+	while (pCur != NULL) {
+		// save data in output stream
+		wComplete << pCur->GetcarNum() << '\t' 
+			<< pCur->GetcarOwner() << '\t'
+			<< pCur->Getstate() << '\n';
+
+		pCur = pCur->GetNext();	// pCur points the next node
+	}
+	
+	wComplete.close();
+	return true;
+}
